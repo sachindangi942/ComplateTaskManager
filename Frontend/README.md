701f7f14c62e74861e1aae785c90e2b2fdeab876
isd9194 <125755910+isd9194@users.noreply.github.com>
2024-12-03 17:46:09 +0530

    Frontend with redux and redux presist
    in this updated code the header will be seprated for admin and login

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# task_mangement
-this is task mangement system 
-=======
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -72,4 +68,3 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
->>>>>>> 8a85e65 (this is react code)
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,50 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Registration from "./UserComponent/Registration";
-import MyLogin from "./UserComponent/Login";
-
-// import Footer from '../GlobalComponent/Footer';
-import NotFound from './NotFound';
-// import ProductAdd from "./ProductComponent/ProductAdd";
-import AddTask from './Task/AddTask';
-import Header from './GlobalComponent/Header';
-import ViewTask from './Task/ViewTask';
-import Dashboard from './GlobalComponent/Dashboard';
-import CreateUser from './UserComponent/CreateUser';
-import UserTask from './Task/UserTask';
+import { Provider } from "react-redux"
+import redux_store, { persist_store } from "./slices/store"
+import FetchReduxData from "./FetchReduxData"
+import { PersistGate } from "redux-persist/integration/react";
 
 
-
-//================ Route session=====================
 const App = () => {
-
-    return (<Router>
-
-        <Routes>
-             <Route path='/usertask' element={<UserTask />} />
-
-             <Route path='/createuser' element={<CreateUser/>} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/list' element={<ViewTask />} />
-            <Route path='/registration' element={<Registration />} />
-
-            <Route path='/registration/login' element={<MyLogin />} />
-            <Route path="/login" element={<MyLogin />} />
-
-            <Route path='task' element={<AddTask />} />
-
-
-            <Route path='/' element={<Registration />} />
-
-
-
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-
-    </Router>
-    )
+   return(
+    <Provider store={redux_store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persist_store}>
+      <FetchReduxData />
+      </PersistGate>
+      
+    </Provider>
+   )
 };
-export default App;
-
-//================ Route session=====================
 
 
+export default App ; 
